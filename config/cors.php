4988e0750550cb47d5386a1443a98a5f8616cbf0
MarshMellows <jobertnoyad93@gmail.com>
2026-05-10 07:49:43 +0800

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => ['https://nuyadjobert.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
